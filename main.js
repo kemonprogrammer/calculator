c735de9983b9443e53dc0b94bb881676a0aed29d
Michael Luu <michaelluu02@protonmail.com>
2021-05-28 15:33:01 +0200

    Add functionality to basic math operators and enter button

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const display = document.querySelector("#display");
-const operators = Array.from(document.querySelectorAll(".operator"));
 const numbers = Array.from(document.querySelectorAll(".number"));
+const operators = Array.from(document.querySelectorAll(".operator"));
+const clear = document.querySelector("#clear");
+const enter = document.querySelector("#enter");
 
+
+let firstNumber = 0;
+let secondNumber = 0;
+let sign = "";
+let result = 0;
+let equation = "";
+
+display.value = "";
 
 const add = (a, b) => {
   return +a + +b;
@@ -20,12 +30,53 @@ const divide = (a, b) => {
 }
 
 
-display.value = "";
 
 numbers.forEach(number => {
   number.addEventListener("click", e => {
-    display.value += number.textContent;
+    if (sign === "") {
+      equation += number.textContent;
+      display.value = equation;
+    } else {
+      secondNumber += number.textContent;
+      equation += number.textContent;
+      display.value = equation;
+    }
   });
+})
+
+clear.addEventListener("click", () => {
+  firstNumber = 0;
+  secondNumber = 0;
+  equation = "";
+  display.value = equation;
+})
+
+operators.forEach(operator => {
+  operator.addEventListener("click", () => {
+    firstNumber = Number(display.value);
+    sign = operator.textContent;
+    equation += sign;
+    display.value = equation;
+  })
+})
+
+enter.addEventListener("click", () => {
+
+  if (sign === "+") {
+    result = add(firstNumber, secondNumber)
+  } else if (sign === "-") {
+    result = substract(firstNumber, secondNumber)
+  } else if (sign === "*") {
+    result = multiply(firstNumber, secondNumber)
+  } else if (sign === "/") {
+    result = divide(firstNumber, secondNumber)
+  } else {
+    alert("Error");
+  }
+  display.value = result;
+  firstNumber = result;
+  equation = firstNumber;
+  sign = "";
 })
 
 let a = 0,
